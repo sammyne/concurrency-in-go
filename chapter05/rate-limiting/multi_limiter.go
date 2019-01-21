95d31f657db972077c7511023e5878801a237bf3
sammyne <xiangminli@alumni.sjtu.edu.cn>
2019-01-21 08:49:42 +0800

    add an aggregated limiter

---
+++ chapter05/rate-limiting/multi_limiter.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"sort"
+
+	"golang.org/x/time/rate"
+)
+
+type RateLimiter interface { // <1>
+	Wait(context.Context) error
+	Limit() rate.Limit
+}
+
+func MultiLimiter(limiters ...RateLimiter) *multiLimiter {
+	byLimit := func(i, j int) bool {
+		return limiters[i].Limit() < limiters[j].Limit()
+	}
+	sort.Slice(limiters, byLimit) // <2>
+	return &multiLimiter{limiters: limiters}
+}
+
+type multiLimiter struct {
+	limiters []RateLimiter
+}
+
+func (l *multiLimiter) Wait(ctx context.Context) error {
+	for _, l := range l.limiters {
+		// why not just the longest???
+		if err := l.Wait(ctx); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (l *multiLimiter) Limit() rate.Limit {
+	return l.limiters[0].Limit() // <3>
+}
