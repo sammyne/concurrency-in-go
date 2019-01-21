2d088d29f01ce449717556443a2a0cbeac2a68bb
sammyne <xiangminli@alumni.sjtu.edu.cn>
2019-01-21 08:36:51 +0800

    a api impl as main.go without rate-limit

--- chapter05/rate-limiting/main.go
+++ chapter05/rate-limiting/main.go
@@ -1,3 +1,42 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"log"
+	"os"
+	"sync"
+)
+
+func main() {
+	defer log.Printf("Done.")
+	log.SetOutput(os.Stdout)
+	log.SetFlags(log.Ltime | log.LUTC)
+
+	apiConnection := Open()
+	var wg sync.WaitGroup
+	wg.Add(20)
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			defer wg.Done()
+			err := apiConnection.ReadFile(context.Background())
+			if err != nil {
+				log.Printf("cannot ReadFile: %v", err)
+			}
+			log.Printf("ReadFile")
+		}()
+	}
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			defer wg.Done()
+			err := apiConnection.ResolveAddress(context.Background())
+			if err != nil {
+				log.Printf("cannot ResolveAddress: %v", err)
+			}
+			log.Printf("ResolveAddress")
+		}()
+	}
+
+	wg.Wait()
+}
